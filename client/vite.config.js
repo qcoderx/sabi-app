bc49f7b66b139581e4b806e4775341a9fdd7d7c3
qcoderx <qlasisi15@gmail.com>
2025-09-25 14:26:57 +0100

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // <--- IMPORT THE PLUGIN
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // ADD THE PLUGINS ARRAY
-  plugins: [react()], 
-
-  // KEEP THIS LINE FOR CORRECT PATHS
-  base: './',
-
+  // Add this line
+  base: './', 
+  
   server: {
     host: true,
     port: 5173,
-    // Add this proxy configuration
     proxy: {
       '/api': {
         target: 'http://localhost:5000', // Your backend server URL
