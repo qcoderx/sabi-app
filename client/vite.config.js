7273ab18c042eebd932fcb78fae41c535d8ba498
qcoderx <qlasisi15@gmail.com>
2025-09-27 21:37:18 +0100

    sigh

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // 1. Import the plugin
 
 export default defineConfig({
   // This ensures that all asset paths in the built index.html are absolute (e.g., /assets/index.js)
   // This is crucial for react-router-dom to work on any URL.
   base: '/', 
+  plugins: [react()], // 2. Use the plugin
   
   server: {
     host: true,
